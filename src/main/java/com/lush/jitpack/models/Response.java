4be1665724ff12aa959e04c510d84865f9a3bc88
is <is@lush.co.kr>
2018-03-21 19:54:38 +0900

    Add lombok library and response model.

---
+++ src/main/java/com/lush/jitpack/models/Response.java
@@ -0,0 +1,57 @@
+package com.lush.jitpack.models;
+
+import lombok.Data;
+
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class Response {
+
+  /**
+   * Response status.
+   */
+  private String status;
+
+  /**
+   * Response message.
+   */
+  private String message;
+
+  /**
+   * Response data.
+   */
+  private Object data;
+
+  /**
+   * Description : Default constructor.
+   */
+  public Response () {
+    this.status = "ok";
+    this.message = "";
+    this.data = "";
+  }
+
+  /**
+   * Description : Constructor.
+   *
+   * @param status
+   */
+  public Response(String status) {
+    this.status = status;
+    this.message = "";
+    this.data = "";
+  }
+
+  /**
+   * Description : Constructor.
+   *
+   * @param status
+   * @param message
+   */
+  public Response(String status, String message) {
+    this.status = status;
+    this.message = message;
+    this.data = "";
+  }
+}
