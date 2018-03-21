ffcd8abab6fbebea32e1e4b292db4081a673ccb8
is <is@lush.co.kr>
2018-03-21 17:36:17 +0900

    Add swagger2 library and config.

---
+++ src/main/java/com/lush/jitpack/configs/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.lush.jitpack.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+  /**
+   * Method name : postsApi.
+   * Description : Get swagger2 information of apis.
+   *
+   * @return springfox.documentation.spring.web.plugins.Docket
+   */
+  @Bean
+  public Docket postsApi() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.lush.controllers"))
+        .paths(PathSelectors.any())
+        .build()
+        .apiInfo(apiInfo());
+  }
+
+  /**
+   * Method name : apiInfo.
+   * Description : Get information of api configuration.
+   *
+   * @return springfox.documentation.service.ApiInfo.
+   */
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+        .version("0.1.0")
+        .title("Microservice Base")
+        .description("A microservice")
+        .build();
+  }
+}
